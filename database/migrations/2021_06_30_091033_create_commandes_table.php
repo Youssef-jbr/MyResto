34e1f77c58deae7d1def11058d25fae46a1bc84b
Aldam91 <59729635+Aldam91@users.noreply.github.com>
2021-06-30 12:06:04 +0200

    Migration
    Migrations + Relations

---
+++ database/migrations/2021_06_30_091033_create_commandes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('mode_livraison');
+            $table->integer('prix');
+            $table->timestamps();
+
+            //Foreign keys
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commandes');
+    }
+}
