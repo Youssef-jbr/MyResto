d6eb54a94c914c60eda2ec000068989fc0fb4d8c
Greygoose78 <grzverev@gmail.com>
2021-07-02 11:51:23 +0200

    login register mobile

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,17 +1,12 @@
 <nav class="bg-black fixed-top">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div class="relative flex items-center justify-between h-16">
-            <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div class="flex items-center justify-start sm:items-stretch ">
                 <div class="flex-shrink-0 flex items-center">
                     <a class="navbar-brand ml-3" href="{{ route('home') }}"><i class="fas fa-utensils fa-lg text-indigo-600"></i></a>
                 </div>
-                <div class="hidden sm:block sm:ml-6">
-                    <div class="flex space-x-4">
-
-                    </div>
-                </div>
             </div>
-            <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div class="absolute inset-y-0 right-0 flex items-center pr-2 static sm:static">
                 @auth
                     <div @click.away="open = false" class="relative" x-data="{ open: false }">
                         <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
@@ -28,17 +23,15 @@
                         </div>
                     </div>
                 @else
-                    <div class="absolute top-0 right-0 mt-4 mr-4">
-                        @if (Route::has('login'))
-                            <div class="space-x-4">
-                                <a href="{{ route('login') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Log in</a>
+                    @if (Route::has('login'))
+                        <div class="space-x-4">
+                            <a href="{{ route('login') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Log in</a>
 
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Register</a>
-                                @endif
-                            </div>
-                        @endif
-                    </div>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Register</a>
+                            @endif
+                        </div>
+                    @endif
                 @endauth
             </div>
         </div>
